98cea29fd1f110b151016b94e2a8dca59c44d3d7
suvam prasad <tituprasad30@gmail.com>
2020-06-02 12:14:18 +0530

    todo class added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './models/todo.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -12,7 +14,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Todo extends StatelessWidget {
+class Todo extends StatefulWidget {
+  @override
+  _TodoState createState() => _TodoState();
+}
+
+class _TodoState extends State<Todo> {
+  final todo = List.generate(
+    20,
+    (index) => TodoList('Todo $index',
+        'A description of what needs to be done for Todo $index'),
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ lib/models/todo.dart
@@ -0,0 +1,6 @@
+class TodoList {
+  final String title;
+  final String description;
+
+  TodoList(this.title, this.description);
+}
